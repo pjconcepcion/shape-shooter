92bf0893a7bc3dd134185656c2a3b0e40fdb2df3
pjconcepcion <patrickjosephconcepcion@gmail.com>
2019-10-26 06:07:15 +0800

    Done issues 1-2-3

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject _player;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+        
+        if (_player == null)
+        {
+            Debug.LogError("Player not found.");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + 3, -10);
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int _lifePoints = 5;
+
+    [SerializeField]
+    private float _speed = 5.0f;
+
+    [SerializeField]
+    private float _jumpHeight = 5.0f;
+
+    private Rigidbody rb;
+
+    private bool _canJump = true;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody not found.");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && _canJump)
+        {
+            rb.velocity = new Vector3(0, _jumpHeight, 0);
+            rb.AddForce(rb.velocity);
+            _canJump = false;
+        }
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(horizontal, 0, 0) * _speed * Time.deltaTime);
+        if(_canJump)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -12f, 12f), transform.position.y, 0);
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.down, 0.5f))
+        {
+            _canJump = true;
+        }
+    }
+
+    
+}
